f2790cd5a32eb418ddc5b6015fce905333e8b463
sikaneka <snehasureshndh@gmail.com>
2024-05-03 21:42:39 +0530

    splash file added

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,91 +1,61 @@
-<<<<<<< HEAD
-// import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
-=======
 
 
 import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
->>>>>>> 93c4b1c4d65acbe353ebd00f8355db67e755408d
 import 'package:flutter/material.dart';
 import 'package:project/auth/login.dart';
-<<<<<<< HEAD
-=======
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:project/heartrate.dart';
->>>>>>> 93c4b1c4d65acbe353ebd00f8355db67e755408d
 import 'package:project/provider/control.dart';
+import 'package:project/splash.dart';
 import 'package:provider/provider.dart';
 
-Future<void> main() async {
-  if (!kIsWeb) {
+
+ Future<void> main() async {
+  if(!kIsWeb) {
     WidgetsFlutterBinding.ensureInitialized();
 
     
     await Firebase.initializeApp();
     runApp(CarePat());
-  } else {
+  }else {
     await Firebase.initializeApp(
-        options: const FirebaseOptions(
-            apiKey: "AIzaSyCIghJx7VkeANVc4MkOp5R9LIdr52Ryq5o",
-            authDomain: "care-pat.firebaseapp.com",
-            databaseURL: "https://care-pat-default-rtdb.firebaseio.com",
-            projectId: "care-pat",
-            storageBucket: "care-pat.appspot.com",
-            messagingSenderId: "1008121233833",
-            appId: "1:1008121233833:web:3226f85b40975bcfd284b8",
-            measurementId: "G-XWGCLLX8NZ"));
-  }
-  runApp(const CarePat());
+         options:const FirebaseOptions(
+         apiKey: "AIzaSyCIghJx7VkeANVc4MkOp5R9LIdr52Ryq5o",
+         authDomain: "care-pat.firebaseapp.com",
+         databaseURL: "https://care-pat-default-rtdb.firebaseio.com",
+         projectId: "care-pat",
+         storageBucket: "care-pat.appspot.com",
+         messagingSenderId: "1008121233833",
+         appId: "1:1008121233833:web:3226f85b40975bcfd284b8",
+         measurementId: "G-XWGCLLX8NZ"
+        ));}
+    runApp(const CarePat());
+
 }
+
 
 class CarePat extends StatelessWidget {
   const CarePat({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
+    return  MultiProvider(
+
       providers: [
-        ChangeNotifierProvider(
-          create: (context) => Loginprovider(),
-        ),
-      ],
+        
+       ChangeNotifierProvider(create: (context) => Loginprovider(),),
+        
+        ],
+
       child: MaterialApp(
         title: 'care_pat',
-        // theme: ThemeData(
-        //   primarySwatch: Colors.blue,
-
-        // ),
-        home: LoginForm(),
-        // home:HeartRate(),
+        
+        home:SplashScreen(),
+        
       ),
     );
   }
 }
 
 
-/*void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        home: StreamBuilder(
-      stream: FirebaseAuth.instance.authStateChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Home();
-        } else {
-          return LoginForm();
-        }
-      },
-    ));
-  }
-}*/
