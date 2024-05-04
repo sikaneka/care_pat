725d7f95dc58fe434611378a575a2945b4ae9c15
HibaTasnim <hibatasnim09@gmail.com>
2024-05-04 19:15:14 +0530

    errror

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,29 +1,29 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:project/provider/control.dart';
 import 'package:project/splash.dart';
 import 'package:provider/provider.dart';
 
- Future<void> main() async {
-  if(!kIsWeb) {
+Future<void> main() async {
+  if (!kIsWeb) {
     WidgetsFlutterBinding.ensureInitialized();
 
     await Firebase.initializeApp();
     runApp(CarePat());
-  }else {
+  } else {
     await Firebase.initializeApp(
-         options:const FirebaseOptions(
-         apiKey: "AIzaSyCIghJx7VkeANVc4MkOp5R9LIdr52Ryq5o",
-         authDomain: "care-pat.firebaseapp.com",
-         databaseURL: "https://care-pat-default-rtdb.firebaseio.com",
-         projectId: "care-pat",
-         storageBucket: "care-pat.appspot.com",
-         messagingSenderId: "1008121233833",
-         appId: "1:1008121233833:web:3226f85b40975bcfd284b8",
-         measurementId: "G-XWGCLLX8NZ"
-        ));}
-    runApp(const CarePat());
-
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCIghJx7VkeANVc4MkOp5R9LIdr52Ryq5o",
+            authDomain: "care-pat.firebaseapp.com",
+            databaseURL: "https://care-pat-default-rtdb.firebaseio.com",
+            projectId: "care-pat",
+            storageBucket: "care-pat.appspot.com",
+            messagingSenderId: "1008121233833",
+            appId: "1:1008121233833:web:3226f85b40975bcfd284b8",
+            measurementId: "G-XWGCLLX8NZ"));
+  }
+  runApp(const CarePat());
 }
 
 class CarePat extends StatelessWidget {
@@ -31,19 +31,15 @@ class CarePat extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MultiProvider(
-
+    return MultiProvider(
       providers: [
-        
-       ChangeNotifierProvider(create: (context) => Loginprovider(),),
-        
-        ],
-
+        ChangeNotifierProvider(
+          create: (context) => Loginprovider(),
+        ),
+      ],
       child: MaterialApp(
         title: 'care_pat',
-        
-        home:SplashScreen(),
-        
+        home: SplashScreen(),
       ),
     );
   }
